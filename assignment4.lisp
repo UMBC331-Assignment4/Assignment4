e34c6c2f57c33722321ca57708daac40f4be1a5f
ayebear <ayebear@gmail.com>
2014-10-23 20:28:50 -0400

    Added comments on algorithm

---
+++ assignment4.lisp
@@ -0,0 +1,6 @@
+; Read command line input
+
+; Recursive algorithm to get all possible combinations of the operators
+
+; Evaluate each list, stopping when one is true
+; If none are true, then print "No equation found"
