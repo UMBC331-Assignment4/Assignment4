411c7275a506248af1eefb8a6bc7ef7f3bc11a3f
ayebear <ayebear@gmail.com>
2014-10-28 13:12:32 -0400

    Added some example code found online

--- assignment4.lisp
+++ assignment4.lisp
@@ -4,3 +4,40 @@
 
 ; Evaluate each list, stopping when one is true
 ; If none are true, then print "No equation found"
+
+; All of this code was copied from http://picolisp.com/wiki/?99pp93
+
+(defun infix (E)
+   (if (atom E)
+      E
+      (list
+         (infix (cadr E))
+         (car E)
+         (infix (caddr E)) ) ) )
+
+(defun expressions (X)
+   (if (cdr X)
+      (mapcan
+         '((I)
+            (mapcan
+               '((A)
+                  (mapcan
+                     '((B)
+                        (mapcar
+                           '((Op) (list Op A B))
+                           '(+ - * /) ) )
+                     (expressions (tail (- I) X)) ) )
+               (expressions (head I X)) ) )
+         (range 1 (dec (length X))) )
+      (list (car X)) ) )
+
+(defun equations (Lst)
+   (use /
+      (redef / (A B)
+         (and (n0 B) (=0 (% A B)) (/ A B)) )
+      (for (I 1  (> (length Lst) I)  (inc I))
+         (for A (expressions (head I Lst))
+            (for B (expressions (tail (- I) Lst))
+               (let? N (eval A)
+                  (when (= N (eval B))
+                     (println (infix A) '= (infix B)) ) ) ) ) ) ) )
